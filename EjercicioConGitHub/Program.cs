1250ae3bdf26a808817c077c484bd81df5f1e841
Daniel Alvarez <dan.alv.ard97@gmail.com>
2022-02-21 13:23:44 +0000

    20220221 En español

--- EjercicioConGitHub/Program.cs
+++ EjercicioConGitHub/Program.cs
@@ -6,7 +6,7 @@ namespace EjercicioConGitHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola Mundo");
         }
     }
 }
