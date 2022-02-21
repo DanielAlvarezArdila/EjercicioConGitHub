be9c22c624d147c3ceabcd5fe0b581102abb6aa9
Alumno <Alumno@DESKTOP-RD5LCFM>
2022-02-21 13:13:05 +0000

    Agregar archivos de proyecto.

---
+++ EjercicioConGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EjercicioConGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
